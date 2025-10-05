987fbeae6cba096e8ada595b41e1381e72a5a758
Adam <elangptra17@gmail.com>
2025-10-05 22:16:59 +0700

    feat: login with JWT token

---
+++ backend-app/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Invalid Credentials'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function me()
+    {
+        return response()->json(JWTAuth::user());
+    }
+
+    public function logout()
+    {
+        JWTAuth::invalidate(JWTAuth::getToken());
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(JWTAuth::refresh(JWTAuth::getToken()));
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type'   => 'bearer',
+            'expires_in'   => JWTAuth::factory()->getTTL() * 60
+        ]);
+    }
+}
---
+++ backend-app/routes/api.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+
+require __DIR__.'/auth.php';
